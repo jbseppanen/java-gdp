89d0addb31db42a419b51aa06191826f64981027
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-14 12:06:55 -0800

    Add CountryNotFoundException and CountryRepository

--- src/main/java/com/lambdaschool/javagdp/Country.java
+++ src/main/java/com/lambdaschool/javagdp/Country.java
@@ -2,15 +2,22 @@ package com.lambdaschool.javagdp;
 
 import lombok.Data;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 @Data
+@Entity
 public class Country {
+
+    private @Id @GeneratedValue Long id;
     private String name;
-    private double gdp;
+    private Long gdp;
 
     public Country() {
     }
 
-    public Country(String name, double gdp) {
+    public Country(String name, Long gdp) {
         this.name = name;
         this.gdp = gdp;
     }
---
+++ src/main/java/com/lambdaschool/javagdp/CountryNotFoundException.java
@@ -0,0 +1,7 @@
+package com.lambdaschool.javagdp;
+
+public class CountryNotFoundException extends RuntimeException {
+    public CountryNotFoundException(Long id) {
+        super("Could not find country");
+    }
+}
