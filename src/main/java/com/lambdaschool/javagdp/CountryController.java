c92ff722e96473b57e98c3f5831a17bad7a6cfad
Jacob Seppanen <jbseppanen@gmail.com>
2019-02-14 13:19:44 -0800

    Add mapping for /economy and /total to Controller Class.

--- src/main/java/com/lambdaschool/javagdp/CountryController.java
+++ src/main/java/com/lambdaschool/javagdp/CountryController.java
@@ -1,5 +1,7 @@
 package com.lambdaschool.javagdp;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 @Slf4j
@@ -23,14 +27,37 @@ public class CountryController {
     }
 
     @GetMapping("/names")
-    public List<Country> getAll() {
+    public List<Country> getAllSortedAlphabetically() {
         ArrayList<Country> countries = (ArrayList<Country>) countryRepo.findAll();
         countries.sort((e1, e2) -> e1.getCountry().compareToIgnoreCase(e2.getCountry()));
         return countries;
     }
 
+    @GetMapping("/economy")
+    public List<Country> getAllSortedByEconomy() {
+        ArrayList<Country> countries = (ArrayList<Country>) countryRepo.findAll();
+        countries.sort((e1, e2)-> e2.getGdp().compareTo(e1.getGdp()));
+        return countries;
+    }
+
+    @GetMapping("/total")
+    public ObjectNode getTotalGdp() {
+        ArrayList<Country> countries = (ArrayList<Country>) countryRepo.findAll();
+        Long total = 0L;
+        for (Country country:countries) {
+            total+=country.getGdp();
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode node = mapper.createObjectNode();
+        node.put("Total", total);
+        return node;
+    }
+
+
     @PostMapping("/names")
     public List<Country> loadCountries(@RequestBody List<Country> countriesToUpload) {
         return countryRepo.saveAll(countriesToUpload);
     }
+
+
 }
